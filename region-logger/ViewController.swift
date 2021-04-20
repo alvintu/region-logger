978eeed2534ffbebb5ca4a0588acff3bb4d6f09e
Alvin <tu.alvinj@gmail.com>
2021-04-20 11:08:02 -0400

    Add mapview and location manager with the respective delegations within extensions

--- region-logger/ViewController.swift
+++ region-logger/ViewController.swift
@@ -6,20 +6,37 @@
 //
 
 import UIKit
-import CoreData
+import MapKit
 
 class ViewController: UIViewController {
+  let mapView = MKMapView()
+  let locationManager = CLLocationManager()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    DataManager.saveCoreData(latitudeVal: 10, longitudeVal: 10)
-    DataManager.saveCoreData(latitudeVal: 1, longitudeVal: 10)
-    DataManager.saveCoreData(latitudeVal: 3, longitudeVal: 10)
-    let thresholds = DataManager.fetchCoreData()
-    print(thresholds)
+    mapView.delegate = self
+    locationManager.delegate = self
+    mapView.frame = view.bounds
+    view.addSubview(mapView)
+    
+//    DataManager.saveCoreData(latitudeVal: 10, longitudeVal: 10)
+//    DataManager.saveCoreData(latitudeVal: 1, longitudeVal: 10)
+//    DataManager.saveCoreData(latitudeVal: 3, longitudeVal: 10)
+//    let thresholds = DataManager.fetchCoreData()
+//    print(thresholds)
+  }
+}
+  
+  extension ViewController: MKMapViewDelegate {
+    //add relevant methods
+    
   }
 
+extension ViewController: CLLocationManagerDelegate {
+  //add relevant methods
+  
+}
   
 
-}
+
 
